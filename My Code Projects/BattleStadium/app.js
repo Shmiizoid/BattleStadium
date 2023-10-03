c26c36394ffad4d0e333e9176b59096bed82e61f
Shmiizoid <eafuentes949@gmail.com>
2023-10-02 20:51:43 -0700

    basics

---
+++ My Code Projects/BattleStadium/app.js
@@ -0,0 +1,26 @@
+// const pokedex = [
+//   { name: "bulbasaur", type: "grass", health: 1, atk: 01 },
+//   { name: "ivysaur", type: "grass", health: 1, atk: 01 },
+//   { name: "venusaur", type: "grass", health: 1, atk: 01 },
+//   { name: "bulbasaur", type: "grass", health: 01, atk: 01 },
+//   { name: "ivysaur", type: "grass", health: 01, atk: 01 },
+//   { name: "venusaur", type: "grass", health: 01, atk: 01 },
+// ];
+
+// const pokedex2 = "this is Pokedex2"
+
+// for (let i=0; i<pokedex2.length; i++) {
+//    console.log(pokedex[i])
+// }
+
+//  console.log(pokedex);
+
+//  console.log([
+//    {name:"bulbasaur", type:"grass", health: 01, atk: 01 },
+//    {name:"ivysaur", type:"grass", health: 01, atk: 01 },
+//    {name:"ivysaur", type:"grass", health: 01, atk: 01 },
+// ])
+
+// pokedex.forEach((element) => {
+//    console.log(element)
+// })
